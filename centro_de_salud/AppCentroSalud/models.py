789bbfb68ca5c6626ca54924a347f99fafa06dd0
Alejandro Scribano <scribanoalejandro@gmail.com>
2021-12-12 18:39:57 -0300

    Models creados

--- centro_de_salud/AppCentroSalud/models.py
+++ centro_de_salud/AppCentroSalud/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+
+class CuerpoMedico(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    especialidad = models.CharField(max_length=30)
+    dia_hora_atencion = models.CharField(max_length=90)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=15)
+    
+class Pacientes(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    documento = models.IntegerField()
+    email = models.EmailField()
+    telefono = models.CharField(max_length=15)
+
+class Consulta(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=15)
+    consulta_paciente = models.CharField(max_length=150)
+
+
